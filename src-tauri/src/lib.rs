a7087845ebd99cb5d4f555b96b4d3d79efb59c09
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-01-28 16:26:30 +0400

    added status bar app menu

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,13 +3,39 @@ mod db;
 mod models;
 
 use commands::*;
+use tauri::menu::{Menu, MenuItem, Submenu, PredefinedMenuItem};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
-            db::initialize_db(&app.handle())?;
+            let app_handle = app.handle().clone();
+
+            let file_menu = Submenu::with_items(
+                &app_handle,
+                "File",
+                true,
+                &[&PredefinedMenuItem::quit(&app_handle, Some("Quit"))?],
+            )?;
+
+            let license_item =
+                MenuItem::with_id(&app_handle, "license", "License", true, None::<&str>)?;
+            let help_menu =
+                Submenu::with_items(&app_handle, "Help", true, &[&license_item])?;
+
+            let menu = Menu::with_items(&app_handle, &[&file_menu, &help_menu])?;
+            app.set_menu(menu)?;
+
+            app.on_menu_event(|_app, event| {
+                if event.id.as_ref() == "license" {
+                    let _ = open::that(
+                        "https://github.com/AlbertArakelyan/OptionLedger/blob/main/LICENSE",
+                    );
+                }
+            });
+
+            db::initialize_db(&app_handle)?;
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
